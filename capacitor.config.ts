ff57cbdec0dd44782a1bf3a76a975aaf50ee55f6
Rahul Sriwastav <rahulsriwastav@rupantra.com>
2025-12-23 07:49:05 +0530

    Change: Update app name from 'Rupantar AI' to 'Rupantra AI'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.rupantar.ai',
-  appName: 'Rupantar AI',
+  appName: 'Rupantra AI',
   webDir: 'out',
   server: {
     androidScheme: 'https'
